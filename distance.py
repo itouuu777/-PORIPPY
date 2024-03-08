891800bfeefbb47b6646eeeefcb3cdc13fdec7c1
keitaroi11 <149392536+keitaroi11@users.noreply.github.com>
2024-03-08 14:50:10 +0900

    Add files via upload

--- distance.py
+++ distance.py
@@ -1,6 +1,9 @@
-import scipy.interpolate as scipl
 import numpy as np
+from scipy.optimize import curve_fit
 import csv
+
+def tenth_order_func(x, a, b, c, d, e, f, g, h, i, j, k):
+    return a * x**10 + b * x**9 + c * x**8 + d * x**7 + e * x**6 + f * x**5 + g * x**4 + h * x**3 + i * x**2 + j * x + k
 
 def distance():
     filename = 'distance_data.csv'
@@ -14,8 +17,10 @@ def distance():
                 camera_data.append(float(row[0]))
                 real_data.append(float(row[1]))
 
-    f_sci=scipl.CubicSpline(camera_data,real_data)
-    
+    popt, pcov = curve_fit(tenth_order_func, camera_data, real_data)
 
+    x_value = 100
+    y_value = tenth_order_func(x_value, *popt)
+    print(y_value)
 
-
+distance()
